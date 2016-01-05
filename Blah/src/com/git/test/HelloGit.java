1faf8fc0a5c862d7161cad5ed96b468db0434715
e3evans <e3evans@gmail.com>
2016-01-05 13:41:25 -0600

    blah

--- Blah/src/com/git/test/HelloGit.java
+++ Blah/src/com/git/test/HelloGit.java
@@ -28,6 +28,7 @@ public class HelloGit extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("BLAH");
 	}
 
 	/**
